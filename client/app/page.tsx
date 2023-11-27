bc044fbe86df317bd1ab7c5f43698e8608181e1e
wmonacho <mimimonach@gmail.com>
2023-11-27 08:23:49 +0100

    gateway try

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -173,10 +173,6 @@ export default function Home() {
 			console.log('Disconnected from the server');
 		})
 
-		socket.on('disconnect', () => {
-			console.log('Disconnected from the server');
-		})
-
 		return () => {
 			console.log('Unregistering events...');
 			userSocket.off('connect');
