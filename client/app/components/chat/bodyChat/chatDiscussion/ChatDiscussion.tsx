85830401f51581061445991034159f1a132cace1
Edouard Brodeur <ebrodeur@z4r5p7.42lyon.fr>
2023-11-27 16:06:35 +0100

    Display fixed

--- client/app/components/chat/bodyChat/chatDiscussion/ChatDiscussion.tsx
+++ client/app/components/chat/bodyChat/chatDiscussion/ChatDiscussion.tsx
@@ -3,13 +3,33 @@ import React, { useState , useEffect } from 'react';
 import { Socket } from 'socket.io-client'
 
 interface Message {
+	from: string;
 	content: string;
+<<<<<<< HEAD
 	date: string;
+=======
+	post_datetime: string;
+	conversationName: string;
+>>>>>>> 61b0433 (Display fixed)
 }
 
 const ChatDiscussionComponent = (socket: {socket: Socket}) => {
 
-	const conversationName = "test";
+	const formatDateTime = (dateTimeString: string) => {
+		const options = {
+		  day: 'numeric',
+		  month: 'numeric',
+		  year: 'numeric',
+		  hour: '2-digit',
+		  minute: '2-digit',
+		  second: '2-digit',
+		};
+	  
+		const formattedDate = new Intl.DateTimeFormat('en-GB', options).format(new Date(dateTimeString));
+		return formattedDate;
+	  };
+
+	const conversationName = "test2";
 	const socketInUse = socket.socket;
 	const [messages, setMessages] = useState<Message[]>([]);
 	const [newMessage, setNewMessage] = useState("");
@@ -22,6 +42,14 @@ const ChatDiscussionComponent = (socket: {socket: Socket}) => {
 		getMessage();
 	});
 
+<<<<<<< HEAD
+=======
+	const isMyMessage = (message: Message): boolean => {
+		return message.from === "ebrodeur";
+	};
+
+	// This function will retreive all the messages from the conversation and set the messages array for display
+>>>>>>> 61b0433 (Display fixed)
 	const getMessage = async () => {
 		
 		try {
@@ -38,12 +66,34 @@ const ChatDiscussionComponent = (socket: {socket: Socket}) => {
 		}
 	}
 
+<<<<<<< HEAD
+=======
+	// Here we retreive the last sent message and we "insert" it in the messages array
+	useEffect(() => {
+		socketInUse.on('onMessage', (message: Message) => {
+			if (message)
+				setMessages((prevMessages: Message[]) => [...prevMessages, message]);
+		});
+		
+		return () => {
+			socketInUse.off('onMessage')
+		}
+	}, [socketInUse]);
+	
+	// Loading the conversation (retrieving all messages on component rendering)
+	useEffect(() => {
+		console.log("Loading conversation...");
+		getMessage();
+	}, []);
+
+>>>>>>> 61b0433 (Display fixed)
 	return (
 		<div className="bloc-discussion-chat">
 			<p className="discussion-chat">{newMessage}</p>
 			{messages.map((message: Message) => (
 				<>
-					<p className="discussion-chat">{message.content}</p>
+					<p className="discussion-chat-content">{message.content}</p>
+					<p className="discussion-chat-date">{formatDateTime(message.post_datetime)}</p>
 				</>
 			))}
 		</div>
