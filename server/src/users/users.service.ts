7afaef913801e3fccae4d1f49d55d1b16ed3854e
Edouard Brodeur <ebrodeur@z4r10p5.42lyon.fr>
2023-12-06 18:02:47 +0100

    on a fait les btos

--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -7,10 +7,7 @@ import { Friendship } from './entities/friendship.entity'
 import { speakeasy } from 'speakeasy'
 import { QRCode } from 'qrcode'
 import { FriendRequestDto } from './dto/FriendRequestDto.dto';
-<<<<<<< HEAD
 import { Conversation } from 'src/chat/entities/conversation.entity';
-=======
->>>>>>> aefff37 (Reworked friendship (isse with dto transmission) + trying to implement notifications)
 
 @Injectable()
 export class UsersService {
@@ -236,5 +233,4 @@ export class UsersService {
 		}
 		return ;
 	}
-
 }
