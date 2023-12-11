7deff7737e5895c99cc7e2a4347e427802b433c5
Edouard Brodeur <ebrodeur@z4r10p5.42lyon.fr>
2023-12-11 17:12:36 +0100

    Removed useless console.log and updated sendBox + receiveBox for both chatUser and chatChannel

--- client/app/components/chat/bodyChat/chatUser/chatList/ChatList.tsx
+++ client/app/components/chat/bodyChat/chatUser/chatList/ChatList.tsx
@@ -42,8 +42,6 @@ const ChatListComponent: React.FC = () => {
 			"on",
 		]
 	}
-	console.log("conversations ============================================== ");
-	console.log(conversations);
 
 	useEffect(() => {
 		console.log("Loading converssations...");
