bc584e5565c0e456fc0c7254255cf91783230d6f
iAkoa <mycomputer@icloud.com>
2023-12-11 11:40:11 +0100

    fix du receivebox

--- client/app/components/chat/bodyChat/chatUser/receiveBox/ReceiveBox.tsx
+++ client/app/components/chat/bodyChat/chatUser/receiveBox/ReceiveBox.tsx
@@ -92,10 +92,6 @@ const ReceiveBoxComponent = (socket: {socket: Socket}) => {
 				</>
 			))}
 		</div>
-		// <div className="bloc-receive-chat">
-		// 	<p className="receive-chat">coucou comment vas tu ? quelles sont tes perfs en sbd ?</p>
-		// 	<p className="receive-chat">70kg au banc pousse 120kg au soulevé de mort et 90 au squat et toi ?</p>
-		// </div>
 	)
 };
 export default ReceiveBoxComponent;
