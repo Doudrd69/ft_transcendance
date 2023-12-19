bef3e41c55d62e00c4477d3f7063846b5c02bb05
William Monachon <86129877+wmonacho@users.noreply.github.com>
2023-12-19 18:50:18 +0000

    create Game duo

--- server/src/game/matchmaking/matchmaking.service.ts
+++ server/src/game/matchmaking/matchmaking.service.ts
@@ -6,81 +6,40 @@ import { InjectRepository } from '@nestjs/typeorm';
 @Injectable()
 export class MatchmakingService {
 
-    public playersQueue: string[];
-
-
-
-    // async getUserByLogin(loginToSearch: string): Promise<User> {
-	// 	return await this.playersRepository.findOne({ where: {login: loginToSearch}});
-	// }
-
+    public playersQueue: string[] = [];
+	
     async getPlayersPairs()  {
-        // Créer des paires
-        const pairs = [];
+		// Créer des paires
+		const pairs: Array<[string, string]> = [];
         for (let i = 0; i < this.playersQueue.length - 1; i += 2) {
             pairs.push([this.playersQueue[i], this.playersQueue[i + 1]]);
+			console.log("one pair FIND");
         }
+		return pairs;
+        // return new Promise((resolve, reject) => {
+		// 	if (pairs.length >= 2) {
+		// 	  resolve(pairs);
+		// 	} else {
+		// 	  reject('Not enough players');
+		// 	}
+		// });
+	}
+		
 
-        return pairs;
+    async join(playerLogin: string) {
+		this.playersQueue.push(playerLogin);
+		console.log("player rejoin the queue");
     }
 
-    async join(playerId: string) {
-        this.playersQueue.push(playerId);
+	async leave(playerLogin: string) {
+        const index = this.playersQueue.findIndex(login => login === playerLogin);
+        if (index !== -1) {
+            this.playersQueue.splice(index, 1);
+            console.log("player leave the queue");
+        }
     }
 
-    async checkPlayersPairs() {
+    async IsThereEnoughPairs() {
         return (this.playersQueue.length >= 2);
     }
-    // async createLobby(lobbyName, playerName: string): Promise<Lobby> {
-    //     console.log("-- Create Lobby --");
-    //     console.log("Lobby ID: ", lobbyName);
-    //     console.log("User ID: ", playerName);
-    //     const newLobby = this.lobbyRepository.create({name : lobbyName});
-    //     this.getUserByLogin(playerName).then(result => {
-    //         newLobby.lobbyTable.push(result);
-    //     }).catch(error => {
-    //         console.log("Error in first promise: ", error);
-    //     })
-    //     this.lobbyRepository.save(newLobby);
-    //     console.log(`The player: ${playerName}, create lobby: ${newLobby.name}.`);
-    //     return ;
-    // }
-    // async checkLobbyAlreadyExist(playerName: string): Promise<Lobby> {
-    
-    //     const allLobbies = await this.lobbyRepository.find({ relations: ['lobbyTable'] });
-        
-    //     for (const lobby of allLobbies) {
-    //         if (lobby.lobbyTable.length < 2) {
-    //             this.getUserByLogin(playerName).then(result => {
-    //                 lobby.lobbyTable.push(result);
-    //             }).catch(error => {
-    //                 console.log("Error in first promise: ", error);
-    //             })
-    //             await this.lobbyRepository.save(lobby);
-    //             console.log(`The player: ${playerName}, join lobby: ${lobby.name}.`);
-    //             return ;
-    //         }
-    //     }
-
-    //     await this.createLobby(playerName + "'s Lobby", playerName);
-    //     return ;
-    // }
-
-    // async leaveLobby(playerName: string)
-    // {
-    //     /*
-    //     parcourir les lobby et trouver ou est le player, puis le 'depush'
-    //     */
-        
-    //     return ;
-    // }
-
-    // async deleteEmptyLobby() 
-    // {
-    //     return ;
-    // }
 }
-
-/**
- * mtn il faut que je supprime le lobby des que le joueur qui l'a cree part
- */
