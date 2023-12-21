17088efa5a30486359069b35b931391aaed73bf2
Edouard Brodeur <ebrodeur@z2r10p2.42lyon.fr>
2023-12-21 09:45:15 +0100

    Add a route + function in chat.service to retreive conversations WITH the rights on it for the current user (admin or not)

--- client/app/components/chat/bodyChat/chatChannel/channelList/ChannelList.tsx
+++ client/app/components/chat/bodyChat/chatChannel/channelList/ChannelList.tsx
@@ -8,53 +8,57 @@ interface Conversation {
   is_channel: boolean;
 }
 
-// Il faudrait rejoin la room de chaque conv apres une deco
 const ChannelListComponent: React.FC = () => {
 
-  const { state, dispatch } = useChat();
+	const { state, dispatch } = useChat();
 
-  const [conversations, setConversations] = useState<Conversation[]>([]);
-  const user = sessionStorage.getItem("currentUserID");
+	const [conversations, setConversations] = useState<Conversation[]>([]);
+	const user = sessionStorage.getItem("currentUserID");
 
-  const loadDiscussions = async () => {
+	const loadDiscussions = async () => {
 
-	const response = await fetch(`http://localhost:3001/chat/getConversations/${user}`, {
-	  method: 'GET',
-	});
+		const response = await fetch(`http://localhost:3001/chat/getConversationsWithStatus/${user}`, {
+			method: 'GET',
+		});
 
-	if (response.ok) {
-		const userData = await response.json();
-		setConversations((prevConversations: Conversation[]) => [...prevConversations, ...userData]);
-	} else {
-	  console.log("Fatal error");
-	}
-  };
+		if (response.ok) {
+			const conversationsData = await response.json();
+			const { conversations, isAdmin } = conversationsData;
+			console.log("==> ", isAdmin);
 
-  const userData = {
-	discussion: conversations,
-	online: ["on", "off", "on", "on", "off", "on", "on"],
-  };
+			setConversations((prevConversations: Conversation[]) => [...prevConversations, ...conversations]);
+			// console.log("Groups => ", conversations);
 
-  useEffect(() => {
-	console.log("Loading conversations...");
-	loadDiscussions();
+		} else {
+			console.log("Fatal error");
+		}
+	};
+
+	const userData = {
+		discussion: conversations,
+		online: ["on", "off", "on", "on", "off", "on", "on"],
+	};
+
+	useEffect(() => {
+		console.log("Loading conversations...");
+		loadDiscussions();
 	}, []);
 
-  return (
-	<div className="bloc-channel-list">
-	  {userData.discussion.map((conversation: Conversation, index: number) => (
-		conversation.is_channel && (
-		  <button key={index} className="button-channel-list" onClick={() => {
-			dispatch({ type: 'TOGGLE', payload: 'showChannel' });
-			dispatch({ type: 'SET_CURRENT_CONVERSATION', payload: conversation.name });
-			dispatch({ type: 'SET_CURRENT_CONVERSATION_ID', payload: conversation.id });
-		  }}>
-			<span>{conversation.name}</span>
-		  </button>
-		)
-	  ))}
-	</div>
-  );
+	return (
+		<div className="bloc-channel-list">
+			{userData.discussion.map((conversation: Conversation, index: number) => (
+			conversation.is_channel && (
+			<button key={index} className="button-channel-list" onClick={() => {
+					dispatch({ type: 'TOGGLE', payload: 'showChannel' });
+					dispatch({ type: 'SET_CURRENT_CONVERSATION', payload: conversation.name });
+					dispatch({ type: 'SET_CURRENT_CONVERSATION_ID', payload: conversation.id });
+		  		}}>
+				<span>{conversation.name}</span>
+		  	</button>
+			)
+	  	))}
+		</div>
+  	);
 };
 
 export default ChannelListComponent;
