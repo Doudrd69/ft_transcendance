3de93737bdea4d6b52b17471f51471539a6f7d7a
William Monachon <86129877+wmonacho@users.noreply.github.com>
2024-01-08 11:06:20 +0100

    Move paddle (#84)
    * some changes
    
    * change front pong for gamebegin
    
    * A user can now add one of his friend into one of his channel --> need to add the friend t the channel's room for socket
    
    * Fixed issue with user not add in room
    
    * refresh-relouad-cache_avatar
    
    * Various fix: inputs protection, socker reconnection...
    
    * Fix + prepare back for  new channel update
    
    * 23bricole
    
    * gerer
    
    * sacompilerpas
    
    * messagelist presque good
    
    ---------
    
    Co-authored-by: Edouard Brodeur <ebrodeur@z1r11p3.42lyon.fr>
    Co-authored-by: iAkoa <mycomputer@icloud.com>
    Co-authored-by: Edouard Brodeur <edouard@edouards-macbook-pro.home>

--- client/app/components/game/gamePong/GamePong.tsx
+++ client/app/components/game/gamePong/GamePong.tsx
@@ -4,10 +4,12 @@ import { useGame } from '../GameContext'
 import { Socket } from 'socket.io-client'
 
 const PongComponent = (socket: { socket: Socket }) => {
-    
+
     const { state, dispatch } = useGame();
     const gameSocket = socket.socket;
 
+    const [countdown, setCountdown] = useState<number>(3);
+    const [blurGame, setBlurGame] = useState<boolean>(true);
     const [ballX, setBallX] = useState<number>(50);
     const [ballY, setBallY] = useState<number>(50);
     const [ballSpeedX, setBallSpeedX] = useState<number>(0.5);
@@ -24,6 +26,83 @@ const PongComponent = (socket: { socket: Socket }) => {
     const [isSKeyPressed, setIsSKeyPressed] = useState<boolean>(false);
     const [isWKeyPressed, setIsWKeyPressed] = useState<boolean>(false);
 
+
+    const updateBallPosition = () => {
+        setBallX((prevBallX) => prevBallX + ballSpeedX + ballSpeed);
+        setBallY((prevBallY) => prevBallY + ballSpeedY + ballSpeed);
+
+        if (ballX <= 26) {
+            setBallX(50);
+            setBallY(50);
+            setScorePlayer2((prevScore) => prevScore + 1);
+            setBallSpeedX((prevSpeedX) => -prevSpeedX);
+        }
+        else if (ballX >= 98) {
+            setBallX(50);
+            setBallY(50);
+            setScorePlayer1((prevScore) => prevScore + 1);
+            setBallSpeedX((prevSpeedX) => -prevSpeedX);
+        }
+
+        if (ballY <= 10) {
+            setBallY(11);
+            setBallSpeedY((prevSpeedY) => -prevSpeedY);
+        }
+        else if (ballY >= 97) {
+            setBallY(96);
+            setBallSpeedY((prevSpeedY) => -prevSpeedY);
+        }
+
+        if (ballX <= 29 && ballY >= paddleY && ballY <= paddleY + 20 && ballSpeedX < 0) {
+            setBallSpeedX((prevSpeedX) => -prevSpeedX);
+        }
+        if (ballX >= 95 && ballY >= paddleY1 && ballY <= paddleY1 + 20 && ballSpeedX > 0) {
+            setBallSpeedX((prevSpeedX) => -prevSpeedX);
+        }
+
+        if (isSKeyPressed) {
+            setPaddleY((prevY) => Math.min(90, prevY + paddleSpeed)); // Empêcher de descendre hors écran
+        }
+
+        if (isWKeyPressed) {
+            setPaddleY((prevY) => Math.max(10, prevY - paddleSpeed));
+        }
+
+        if (isKeyDownPressed) {
+            setPaddleY1((prevY) => Math.min(90, prevY + paddleSpeed)); // Déplacer vers le bas
+        }
+
+        if (isKeyUpPressed) {
+            setPaddleY1((prevY) => Math.max(10, prevY - paddleSpeed)); // Déplacer vers le haut
+        }
+    }
+
+    useEffect(() => {
+        const startCountdown = () => {
+            setBlurGame(true);
+
+            const countdownInterval = setInterval(() => {
+                setCountdown((prevCountdown) => prevCountdown - 1);
+            }, 1000);
+
+            setTimeout(() => {
+                clearInterval(countdownInterval);
+                setCountdown(3);
+                setBlurGame(false); // Arrêter le flou
+            }, 3000);
+        };
+
+        const gameLoop = setInterval(() => {
+            if (!blurGame) {
+                updateBallPosition();
+            }
+        }, 16);
+
+        return () => {
+            clearInterval(gameLoop);
+        };
+    }, [blurGame]);
+
     const handleKeyDown = (e: KeyboardEvent) => {
         if (e.key === 'ArrowUp') {
             setIsKeyUpPressed(true);
@@ -31,7 +110,7 @@ const PongComponent = (socket: { socket: Socket }) => {
             setIsKeyDownPressed(true);
         }
     };
-    
+
     const handleKeyUp = (e: KeyboardEvent) => {
         if (e.key === 'ArrowUp') {
             setIsKeyUpPressed(false);
@@ -43,6 +122,7 @@ const PongComponent = (socket: { socket: Socket }) => {
     const handleKeyS = (e: KeyboardEvent) => {
         if (e.key === 'w') {
             setIsWKeyPressed(true);
+            // gameSocket.emit()
         } else if (e.key === 's') {
             setIsSKeyPressed(true);
         }
@@ -62,62 +142,63 @@ const PongComponent = (socket: { socket: Socket }) => {
         window.addEventListener('keyup', handleKeyW);
         window.addEventListener('keydown', handleKeyS);
 
-        
-const updateBallPosition = () => {
-    setBallX((prevBallX) => prevBallX + ballSpeedX + ballSpeed);
-    setBallY((prevBallY) => prevBallY + ballSpeedY + ballSpeed);
 
-    if (ballX <= 26) {
-        setBallX(50);
-        setBallY(50);
-        setScorePlayer2((prevScore) => prevScore + 1);
-        setBallSpeedX((prevSpeedX) => -prevSpeedX);
-    }
-    else if (ballX >= 98) {
-        setBallX(50);
-        setBallY(50);
-        setScorePlayer1((prevScore) => prevScore + 1);
-        setBallSpeedX((prevSpeedX) => -prevSpeedX);
-    }
+        const updateBallPosition = () => {
+            setBallX((prevBallX) => prevBallX + ballSpeedX + ballSpeed);
+            setBallY((prevBallY) => prevBallY + ballSpeedY + ballSpeed);
 
-    if (ballY <= 10) {
-        setBallY(11);
-        setBallSpeedY((prevSpeedY) => -prevSpeedY);
-    }
-    else if (ballY >= 97) {
-        setBallY(96);
-        setBallSpeedY((prevSpeedY) => -prevSpeedY);
-    }
+            if (ballX <= 26) {
+                setBallX(50);
+                setBallY(50);
+                setScorePlayer2((prevScore) => prevScore + 1);
+                setBallSpeedX((prevSpeedX) => -prevSpeedX);
+            }
+            else if (ballX >= 98) {
+                setBallX(50);
+                setBallY(50);
+                setScorePlayer1((prevScore) => prevScore + 1);
+                setBallSpeedX((prevSpeedX) => -prevSpeedX);
+            }
 
-    if (ballX <= 29 && ballY >= paddleY && ballY <= paddleY + 20 && ballSpeedX < 0) {
-        setBallSpeedX((prevSpeedX) => -prevSpeedX);
-    }
-    if (ballX >= 95 && ballY >= paddleY1 && ballY <= paddleY1 + 20 && ballSpeedX > 0) {
-        setBallSpeedX((prevSpeedX) => -prevSpeedX);
-    }
+            if (ballY <= 10) {
+                setBallY(11);
+                setBallSpeedY((prevSpeedY) => -prevSpeedY);
+            }
+            else if (ballY >= 97) {
+                setBallY(96);
+                setBallSpeedY((prevSpeedY) => -prevSpeedY);
+            }
 
-    if (isSKeyPressed) {
-        setPaddleY((prevY) => Math.min(90, prevY + paddleSpeed)); // Empêcher de descendre hors écran
-    }
+            if (ballX <= 29 && ballY >= paddleY && ballY <= paddleY + 20 && ballSpeedX < 0) {
+                setBallSpeedX((prevSpeedX) => -prevSpeedX);
+            }
+            if (ballX >= 95 && ballY >= paddleY1 && ballY <= paddleY1 + 20 && ballSpeedX > 0) {
+                setBallSpeedX((prevSpeedX) => -prevSpeedX);
+            }
 
-    if (isWKeyPressed) {
-        setPaddleY((prevY) => Math.max(10, prevY - paddleSpeed));
-    }
+            if (isSKeyPressed) {
+                setPaddleY((prevY) => Math.min(90, prevY + paddleSpeed)); // Empêcher de descendre hors écran
+            }
 
-    if (isKeyDownPressed) {
-        setPaddleY1((prevY) => Math.min(90, prevY + paddleSpeed)); // Déplacer vers le bas
-    }
+            if (isWKeyPressed) {
+                setPaddleY((prevY) => Math.max(10, prevY - paddleSpeed));
+            }
 
-    if (isKeyUpPressed) {
-        setPaddleY1((prevY) => Math.max(10, prevY - paddleSpeed)); // Déplacer vers le haut
-    }
-};
+            if (isKeyDownPressed) {
+                setPaddleY1((prevY) => Math.min(90, prevY + paddleSpeed)); // Déplacer vers le bas
+            }
+
+            if (isKeyUpPressed) {
+                setPaddleY1((prevY) => Math.max(10, prevY - paddleSpeed)); // Déplacer vers le haut
+            }
+        };
 
         const gameLoop = setInterval(updateBallPosition, 16);
 
+
         return () => {
             clearInterval(gameLoop);
-            window.removeEventListener('keydown', handleKeyDown); 
+            window.removeEventListener('keydown', handleKeyDown);
             window.removeEventListener('keyup', handleKeyUp);
             window.removeEventListener('keyup', handleKeyW);
             window.removeEventListener('keydown', handleKeyS);
@@ -125,7 +206,10 @@ const updateBallPosition = () => {
     }, [ballX, ballY, ballSpeedX, ballSpeedY, ballSpeed, paddleY, paddleY1, isSKeyPressed, isWKeyPressed]);
 
     return (
-        <div className="pong-container" tabIndex={0}>
+        <div className={`pong-container ${blurGame ? 'game-blur' : ''}`} tabIndex={0}>
+            {countdown > 0 && (
+                <div className="countdown-container">{countdown}</div>
+            )}
             <div className="scoreboard">
                 <div className="col-heading">
                     <h1>Player 1</h1>
@@ -140,7 +224,7 @@ const updateBallPosition = () => {
             <div className="pongpaddle" style={{ top: `${paddleY}%`, left: '26%' }}></div>
             <div className="pongpaddle" style={{ right: 0, top: `${paddleY1}%` }}></div>
         </div>
-    );  
+    );
 };
 
 export default PongComponent;
--- server/src/game_gateway/game.gateway.ts
+++ server/src/game_gateway/game.gateway.ts
@@ -86,12 +86,20 @@ export class GameGateway {
 
     @SubscribeMessage('move-paddle')
     async handlePaddleMove(client: Socket, game: Game) {
+        const PositionPaddle: number = 90;
         if (client.id == game.playerOneID)
-        {
-            //move paddle left
+        {   
+            this.server.to([game.playerOneID,game.playerTwoID]).emit('update-paddle-left', {
+                gameId: this.game.gameId,
+                playerOneID: this.game.playerOneID,
+                playerTwoID: this.game.playerTwoID,
+                scoreOne: this.game.scoreOne,
+                scoreTwo: this.game.scoreTwo,
+                positionPaddleLeft: PositionPaddle,
+            });
         }
         if (client.id == game.playerTwoID)
-        {
+        {   
             //move paddle right
         }
     }
