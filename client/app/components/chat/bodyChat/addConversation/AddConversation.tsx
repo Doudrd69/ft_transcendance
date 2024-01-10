57d553484ed692cae85a59eb29cd92069bd209f1
iAkoa <mycomputer@icloud.com>
2024-01-10 16:24:20 +0100

    dsf

--- client/app/components/chat/bodyChat/addConversation/AddConversation.tsx
+++ client/app/components/chat/bodyChat/addConversation/AddConversation.tsx
@@ -98,11 +98,11 @@ const AddConversationComponent: React.FC<AddConversationComponentProps> = ({ use
 									onChange={(e) => setFormValue(e.target.value)}
 								/>
 								<div className='privated-public'>
-									{isPublic ?
-										<img className='public-img' src="public.png" onClick={() => setIsPublic(false)} />
+									{isPassword ?
+											<img className='password-img' src="passowrd.png" onClick={() => setIsPassowrd(false)} />
 											:
-										<img className='private-img' src="private.png" onClick={() => setIsPublic(true)} />}
-									{!isPublic && (
+											<img className='password-img' src="no-password.png" onClick={() => setIsPassowrd(true)} />}
+									{isPassword && (
 										<input
 											className="add__input"
 											type="password"
