8cde90027be3905bd98927388916532333f8c484
Edouard Brodeur <ebrodeur@z1r11p3.42lyon.fr>
2024-01-16 08:36:15 +0100

    replaced login with username

--- client/app/components/chat/bodyChat/chatFriendsList/friendsList/FriendsList.tsx
+++ client/app/components/chat/bodyChat/chatFriendsList/friendsList/FriendsList.tsx
@@ -103,15 +103,15 @@ const FriendsListComponent: React.FC<FriendsListComponentProps> = ({ userSocket 
 			<div className="tab-and-userclicked" key={id}>
 				<div className="bloc-button-friendslist">
 						<img
-							src={`http://localhost:3001/users/getAvatarByLogin/${friend.friend ? friend.friend.login : friend.initiator ? friend.initiator.login : 'Unknown User'}/${timestamp}`}
+							src={`http://localhost:3001/users/getAvatarByLogin/${friend.friend ? friend.friend.username : friend.initiator ? friend.initiator.username : 'Unknown User'}/${timestamp}`}
 							className={`profil-friendslist ${friend.isActive ? 'on' : 'off'}`}
 							alt="User Avatar"
 						/>
 						<div className={`amies ${activeIndex === id ? 'active' : ''}`} onClick={() => activateTabFriendsList(id)}>
-							{friend.friend ? friend.friend.login : friend.initiator ? friend.initiator.login : 'Unknown User'}
+							{friend.friend ? friend.friend.username : friend.initiator ? friend.initiator.username : 'Unknown User'}
 						</div>
 				</div>
-				{activeIndex === id && <FriendsListTabComponent userSocket={userSocket} userLogin={friend.friend ? friend.friend.login : friend.initiator ? friend.initiator.login : 'Unknown User'} roomName={friend.roomName}  roomID= {friend.roomID}/>}
+				{activeIndex === id && <FriendsListTabComponent userSocket={userSocket} userLogin={friend.friend ? friend.friend.username : friend.initiator ? friend.initiator.username : 'Unknown User'} roomName={friend.roomName}  roomID= {friend.roomID}/>}
 			</div>
 		  ))}
 		</div>
