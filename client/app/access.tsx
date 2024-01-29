6a8f0b617fd6d0e79a771be990ccf59d2e36dd84
Edouard Brodeur <ebrodeur@z1r11p3.42lyon.fr>
2024-01-29 06:30:50 +0100

    test

--- client/app/access.tsx
+++ client/app/access.tsx
@@ -1,23 +1,9 @@
-import { useSearchParams } from 'next/navigation'
-import Image from 'next/image'
-import React, { useState , useEffect } from 'react';
-import RootLayout from './layout'
-import Chat from './components/chat/Chat'
-import Game from './components/game/Game'
-import TFAComponent from './components/TFA/TFAComponent'
-import Header from './components/header/Header'
-import Authentificationcomponent from './components/chat/auth/Authentification';
-import { GameProvider } from './components/game/GameContext';
-import { io, Socket } from 'socket.io-client'
-import { ToastContainer, toast } from 'react-toastify';
+import React from 'react';
 import 'react-toastify/dist/ReactToastify.css';
-import SettingsComponent from './components/settings/Settings';
-import BodyComponent from './components/body/Body';
-import SetComponent from './components/Avatar/SetAvatar';
-import { totalmem } from 'os';
-import GameHeader from './components/game/GameHeader';
 import { useGlobal } from './GlobalContext';
-import { ChatProvider } from './components/chat/ChatContext';
+import TFAComponent from './components/TFA/TFAComponent';
+import Authentificationcomponent from './components/chat/auth/Authentification';
+import Game from './components/game/Game';
 // import { useChat } from './components/chat/ChatContext';
 
 interface Game {
@@ -39,9 +25,8 @@ interface FriendRequestDto {
 const AccessComponent = () => {
 
     const { globalState, dispatch } = useGlobal();
-	//AccessNouspermet de nous diriger vers la bonne fenetre de connexion
+	//Access nous permet de nous diriger vers la bonne fenetre de connexion
 	//ici on regarde si la 2fa est activé ou non
-	console.log("sldkfjsdlkfjasdlkfjasdlkfja");
 	return (
 		<>
 			{!globalState.show2FA && <Authentificationcomponent />}
