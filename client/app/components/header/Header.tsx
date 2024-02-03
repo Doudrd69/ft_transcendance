fb201d6a9f96ccd1d53109548be257b10b0b6f9a
iAkoa <mycomputer@icloud.com>
2024-02-03 04:41:23 +0100

    gerer

--- client/app/components/header/Header.tsx
+++ client/app/components/header/Header.tsx
@@ -8,21 +8,65 @@ import { validate, validateOrReject } from 'class-validator';
 import { useChat } from '../chat/ChatContext';
 
 
+interface FriendShip {
+	id: number;
+	username: string;
+	isBlocked: boolean;
+	onlineStatus: boolean;
+}
+
 const HeaderComponent: React.FC = () => {
 	const {chatDispatch} = useChat();
 	const { globalState, dispatch } = useGlobal();
 	const timestmp = new Date();
+	const [notify, setNotify] = useState<FriendShip[]>([]);
+	const [showActivateNotif, setShowActivateNotif] = useState(false);
 	const uploadAvatar =  () => {
 		dispatch({ type: 'ACTIVATE', payload: 'showUploadAvatar' });
 	}
+	const [newNotifications, setNewNotifications] = useState(false);
 
 	useEffect(() => {
 		uploadAvatar();
 	}, [globalState.showRefresh]);
 
+	const loadNotifications = async() => {
+
+		const response = await fetch(`${process.env.API_URL}/users/getPendingFriends`, {
+			method: 'GET',
+			headers: {
+				'Authorization': `Bearer ${sessionStorage.getItem("jwt")}`,
+			},
+		});
+		if (response.ok) {
+			const notify = await response.json()
+			if (notify)
+				setNotify([...notify]);
+			if (notify.length > 0)
+				setNewNotifications(true);
+			console.log(notify)
+		}
+		else {
+			const error = await response.json();
+			if (Array.isArray(error.message))
+				toast.warn(error.message[0]);
+			else
+				toast.warn(error.message);
+		}
+	}
+
+	useEffect(() => {
+		loadNotifications();
+	}, []);
+
 	const renderComponent = (component: React.ReactNode, condition: boolean) =>
 		condition ? component : null;
-
+	const activateNotif = () =>  {
+		setShowActivateNotif(!showActivateNotif);
+	}
+	const disableNotif = () => {
+		setNewNotifications(false);
+	}
 	return (
 		<div className="header">
 			<div className="bloc-profils">
@@ -47,7 +91,24 @@ const HeaderComponent: React.FC = () => {
 					{/* {renderComponent(<AvatarImageComponent className="profils" refresh={globalState.showRefresh}/>, globalState.showUploadAvatar)} */}
 				</button>
 			</div>
-		<div className="bloc-pong">PONG&CHAT</div>
+		<div className="bloc-pong">PINGPON</div>
+		<button className="button-notifications" onClick={() => {activateNotif(); disableNotif();}}>
+				<div className="notification-icon-container">
+					<img className="notifications-icon" src='./notification.png' alt="Notifications" />
+					{newNotifications && <div className="notification-badge">{notify.length}</div>}
+				</div>
+				{showActivateNotif && (
+					<div className='notif-window'>
+					{notify.map((notif: FriendShip) => (
+						<div className='notif' key={notif.id}>
+						<div className='notif-username'>{notif.username} veut être ton ami </div>
+						<button className='notif-accept' onClick={() => {}}>Accept</button>
+						<button className='notif-decline' onClick={() => {}}>Decline</button>
+						</div>
+					))}
+					</div>
+				)}
+		</button>
 		<div className="bloc-settings">
 			<button
 				className="button-settings"
